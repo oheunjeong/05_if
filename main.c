3ab1b882401ae37c0f3b9c0a2ec96f74eed55856
oheunjeong <55427561+oheunjeong@users.noreply.github.com>
2019-10-08 15:35:11 +0900

    05

--- main.c
+++ main.c
@@ -6,15 +6,11 @@
 int main(int argc, char *argv[]) {
 	
 
-   int i=0, meter;
+   int i;
+   for(i=0;i<10;i++)
+   printf("hello world!\n");
    
-   while (i<3)
-   {
-   	meter = i*1609;
-   	printf("%d 마일은 %d 미터입니다.\n",i,meter);
-   	i ++;
-   }
-
+  
 
 	
 	
